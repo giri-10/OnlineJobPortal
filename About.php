5608307e5cb44056cb1091b32289868440a4f66c
giri-10 <90170719+giri-10@users.noreply.github.com>
2024-03-04 06:54:33 +0530

    initial commit

---
+++ About.php
@@ -0,0 +1,204 @@
+ <section id="content">
+ 	<section class="section-padding">
+ 		<div class="container">
+ 			<div class="row showcase-section">
+ 				<div class="col-md-6">
+ 					<img src="https://media.istockphoto.com/id/1290528901/photo/the-deadline-smashing-department.jpg?s=612x612&w=0&k=20&c=ZDRs6UoURJ6NX96SbUakTczogrWwrauPn-Gr6VB2qjc=" alt="showcase image" height="300" width="500">
+ 				</div>
+ 				<div class="col-md-6">
+ 					<div class="about-text">
+ 						<h3>Career Hub</h3>
+ 						<p>Welcome to Career Hub, your gateway to endless career possibilities and opportunities. At Career Hub, we believe that everyone deserves to find a job they love, and we're here to make that journey easier for you.</p>
+ 						<p>Our mission at Career Hub is simple: to connect job seekers with their ideal careers and empower companies to find top talent effortlessly. We strive to bridge the gap between talented professionals and innovative companies, fostering growth and success for both.</p>
+
+ 					</div>
+ 				</div>
+ 			</div>
+ 		</div>
+ 	</section>
+ 	<div class="container">
+
+ 		<div class="about">
+
+
+ 			<div class="row">
+ 				<div class="col-md-4">
+ 					<!-- Heading and para -->
+ 					<div class="block-heading-two">
+ 						<h3><span>Why Choose Us?</span></h3>
+ 					</div>
+ 					<ul>
+ 						<li>Comprehensive Job Listings: Explore thousands of job openings from leading companies across various industries.</li>
+ 						<li>Personalized Job Recommendations: Receive tailored job recommendations based on your skills, experience, and preferences.</li>
+ 						<li>Professional Development Resources: Access valuable resources, career insights, and expert advice to enhance your professional growth.</li>
+ 						<li>Efficient Recruitment Solutions: Simplify your hiring process with our advanced tools and features designed to streamline recruitment.</li>
+ 					</ul>
+ 				</div>
+ 				<div class="col-md-4">
+ 					<div class="block-heading-two">
+ 						<h3><span>Our Solution</span></h3>
+ 					</div>
+ 					<!-- Accordion starts -->
+ 					<div class="panel-group" id="accordion-alt3">
+ 						<!-- Panel. Use "panel-XXX" class for different colors. Replace "XXX" with color. -->
+ 						<div class="panel">
+ 							<!-- Panel heading -->
+ 							<div class="panel-heading">
+ 								<h4 class="panel-title">
+ 									<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseOne-alt3">
+ 										<i class="fa fa-angle-right"></i> Connecting Talent with Opportunity.
+ 									</a>
+ 								</h4>
+ 							</div>
+ 							<div id="collapseOne-alt3" class="panel-collapse collapse">
+ 								<!-- Panel body -->
+ 								<div class="panel-body">
+ 									Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+ 								</div>
+ 							</div>
+ 						</div>
+ 						<div class="panel">
+ 							<div class="panel-heading">
+ 								<h4 class="panel-title">
+ 									<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseTwo-alt3">
+ 										<i class="fa fa-angle-right"></i> Browse Thousands of Job Listings.
+ 									</a>
+ 								</h4>
+ 							</div>
+ 							<div id="collapseTwo-alt3" class="panel-collapse collapse">
+ 								<div class="panel-body">
+ 									Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+ 								</div>
+ 							</div>
+ 						</div>
+ 						<div class="panel">
+ 							<div class="panel-heading">
+ 								<h4 class="panel-title">
+ 									<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseThree-alt3">
+ 										<i class="fa fa-angle-right"></i> Find Your Dream Career Today.
+ 									</a>
+ 								</h4>
+ 							</div>
+ 							<div id="collapseThree-alt3" class="panel-collapse collapse">
+ 								<div class="panel-body">
+ 									Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+ 								</div>
+ 							</div>
+ 						</div>
+ 						<div class="panel">
+ 							<div class="panel-heading">
+ 								<h4 class="panel-title">
+ 									<a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseFour-alt3">
+ 										<i class="fa fa-angle-right"></i> Explore Career Resources and Tips.
+ 									</a>
+ 								</h4>
+ 							</div>
+ 							<div id="collapseFour-alt3" class="panel-collapse collapse">
+ 								<div class="panel-body">
+ 									Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+ 								</div>
+ 							</div>
+ 						</div>
+ 					</div>
+ 					<!-- Accordion ends -->
+
+ 				</div>
+
+ 				<div class="col-md-4">
+ 					<div class="block-heading-two">
+ 						<h3><span>Our Expertise</span></h3>
+ 					</div>
+ 					<h6>Career Guidance</h6>
+ 					<div class="progress pb-sm">
+ 						<!-- White color (progress-bar-white) -->
+ 						<div class="progress-bar progress-bar-red" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+ 							<span class="sr-only">40% Complete (success)</span>
+ 						</div>
+ 					</div>
+ 					<h6>Domain Experience</h6>
+ 					<div class="progress pb-sm">
+ 						<div class="progress-bar progress-bar-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 90%">
+ 							<span class="sr-only">40% Complete (success)</span>
+ 						</div>
+ 					</div>
+ 					<h6>User Experience</h6>
+ 					<div class="progress pb-sm">
+ 						<div class="progress-bar progress-bar-lblue" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+ 							<span class="sr-only">40% Complete (success)</span>
+ 						</div>
+ 					</div>
+ 					<h6>Interaction</h6>
+ 					<div class="progress pb-sm">
+ 						<div class="progress-bar progress-bar-yellow" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 70%">
+ 							<span class="sr-only">40% Complete (success)</span>
+ 						</div>
+ 					</div>
+ 				</div>
+
+ 			</div>
+
+
+
+ 			<br>
+ 			<!-- Our Team starts -->
+
+ 			<!-- Heading -->
+ 			<div class="block-heading-six">
+ 				<h4 class="bg-color">Our Team</h4>
+ 			</div>
+ 			<br>
+
+ 			<!-- Our team starts -->
+
+ 			<div class="team-six">
+ 				<div class="row">
+ 					<div class="col-md-3 col-sm-6">
+ 						<!-- Team Member -->
+ 						<div class="team-member">
+ 							<!-- Image -->
+ 							<img class="img-responsive" src="img/team1.jpg" alt="">
+ 							<!-- Name -->
+ 							<h4>Johne Doe</h4>
+ 							<span class="deg">Creative</span>
+ 						</div>
+ 					</div>
+ 					<div class="col-md-3 col-sm-6">
+ 						<!-- Team Member -->
+ 						<div class="team-member">
+ 							<!-- Image -->
+ 							<img class="img-responsive" src="img/team2.jpg" alt="">
+ 							<!-- Name -->
+ 							<h4>Jennifer</h4>
+ 							<span class="deg">Programmer</span>
+ 						</div>
+ 					</div>
+ 					<div class="col-md-3 col-sm-6">
+ 						<!-- Team Member -->
+ 						<div class="team-member">
+ 							<!-- Image -->
+ 							<img class="img-responsive" src="img/team3.jpg" alt="">
+ 							<!-- Name -->
+ 							<h4>Christean</h4>
+ 							<span class="deg">CEO</span>
+ 						</div>
+ 					</div>
+ 					<div class="col-md-3 col-sm-6">
+ 						<!-- Team Member -->
+ 						<div class="team-member">
+ 							<!-- Image -->
+ 							<img class="img-responsive" src="img/team4.jpg" alt="">
+ 							<!-- Name -->
+ 							<h4>Kerinele rase</h4>
+ 							<span class="deg">Manager</span>
+ 						</div>
+ 					</div>
+ 				</div>
+ 			</div>
+
+ 			<!-- Our team ends -->
+
+
+ 		</div>
+
+ 	</div>
+ </section>
